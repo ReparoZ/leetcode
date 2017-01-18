2d02a043598ffa8c3475018047b96cff3809392f
FML3426 <fml3426@gmail.com>
2017-01-19 01:26:19 +0800

    P195_Tenth Line

---
+++ shell/P195_Tenth Line/Solution.sh
@@ -0,0 +1,39 @@
+# Source: https://leetcode.com/problems/tenth-line/
+# Author: ML3426
+# Date: 2017-01-17
+
+# How would you print just the 10th line of a file?
+#
+# For example, assume that file.txt has the following content:
+#
+# Line 1
+# Line 2
+# Line 3
+# Line 4
+# Line 5
+# Line 6
+# Line 7
+# Line 8
+# Line 9
+# Line 10
+# Your script should output the tenth line, which is:
+# Line 10
+# [show hint]
+#
+# Hint:
+# 1. If the file contains less than 10 lines, what should you output?
+# 2. There's at least three different solutions. Try to explore all possibilities.
+
+# Solution:
+
+## 使用awk
+## 16ms
+awk 'NR==10' file.txt
+
+## 使用sed
+## 9ms
+sed -n 10p file.txt
+
+## 使用head/tail完成（tail -n +10表示第10行起以后的内容）
+## 9ms
+head -n 10 file.txt | tail -n +10
