270ce9184bed9aec57c38409e898bf2245eb5fdd
FML3426 <fml3426@gmail.com>
2017-02-07 17:08:34 +0800

    P017_Letter Combinations of a Phone Number

--- algorithms/cpp/P017_Letter Combinations of a Phone Number/Solution.cpp
+++ algorithms/cpp/P017_Letter Combinations of a Phone Number/Solution.cpp
@@ -25,7 +25,7 @@ public:
     // 利用迭代的方式获取字符的全排列
     // 时间复杂度为O(3~4^N)，空间复杂度为O(1)
     // Time: 3ms
-    vector<string> letterCombinations(string digits) {
+    vector<string> letterCombinations1(string digits) {
         unordered_map<char, string> mapping = {
                 {'0', ""},
                 {'1', ""},
@@ -55,7 +55,7 @@ public:
         if (digits.length() != 0) {
             char next = digits[0];
             string nextDigits = digits.substr(1, digits.size() - 1);
-            vector<string> lastResult = letterCombinations(mapping, next, nextDigits);
+            vector <string> lastResult = letterCombinations(mapping, next, nextDigits);
             for (int i = 0; i < letters.size(); ++i) {
                 for (int j = 0; j < lastResult.size(); ++j) {
                     result.push_back(letters[i] + lastResult[j]);
@@ -69,6 +69,30 @@ public:
             }
         }
 
+        return result;
+    }
+
+    // 利用循环的方式获取字符的全排列
+    // 时间复杂度为O(3~4^N)，空间复杂度为O(1)
+    // Time: 3ms
+    vector<string> letterCombinations2(string digits) {
+        vector<string> result;
+        if (digits.empty()) return vector<string>();
+        vector <string> v = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        result.push_back("");
+        for (int i = 0; i < digits.size(); ++i) {
+            int num = digits[i] - '0';
+            if (num < 0 || num > 9) break;
+            const string &candidate = v[num];
+            if (candidate.empty()) continue;
+            vector <string> tmp;
+            for (int j = 0; j < candidate.size(); ++j) {
+                for (int k = 0; k < result.size(); ++k) {
+                    tmp.push_back(result[k] + candidate[j]);
+                }
+            }
+            result.swap(tmp);
+        }
         return result;
     }
 };
