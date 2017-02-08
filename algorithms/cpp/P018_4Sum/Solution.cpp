ccc334d6e5bb7399b76b488ab5787766cccd5de2
FML3426 <fml3426@gmail.com>
2017-02-08 14:55:47 +0800

    P018_4Sum

--- algorithms/cpp/P018_4Sum/Solution.cpp
+++ algorithms/cpp/P018_4Sum/Solution.cpp
@@ -1,6 +1,6 @@
-// Source: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+// Source: https://leetcode.com/problems/4sum/
 // Author: ML3426
-// Date: 2017-02-07
+// Date: 2017-02-08
 
 /*
  * Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
