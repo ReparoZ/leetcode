592b8846e5e1d68bbb322d7aab3b9232c8dda3ff
FML3426 <fml3426@gmail.com>
2017-06-18 22:28:24 +0800

    P102_Binary Tree Level Order Traversal

---
+++ algorithms/cpp/P102_Binary Tree Level Order Traversal/Solution.cpp
@@ -0,0 +1,60 @@
+// Source: https://leetcode.com/problems/binary-tree-level-order-traversal/
+// Author: ML3426
+// Date: 2017-06-18
+
+/*
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * return its level order traversal as:
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ */
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    // 一层一层的循环，收集中间结果保存
+    // 时间复杂度为O(N)，空间复杂度为O(N)
+    // Time: 3ms
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        vector<TreeNode *> last;
+        
+        if (root != NULL) last.push_back(root);
+        while (!last.empty()) {
+            vector<TreeNode *> current;
+            vector<int> currentRes;
+            for (int i = 0; i < last.size(); ++i) {
+                currentRes.push_back(last[i]->val);
+                if (last[i]->left != NULL) current.push_back(last[i]->left);
+                if (last[i]->right != NULL) current.push_back(last[i]->right);
+            }
+            res.push_back(currentRes);
+            last = current;
+        }
+        return res;
+    }
+};
